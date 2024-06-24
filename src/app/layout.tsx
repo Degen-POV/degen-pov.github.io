e40c37de50cc821fb5b7cf77c0ac54f0430fe6ab
Lane Babuder <lane@tunedchaos.com>
2024-06-24 14:55:19 -0400

    We're giving a new method a try

---
+++ src/app/layout.tsx
@@ -0,0 +1,55 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css"; // Ensure this is the correct path to your global CSS file
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTelegram,
+  faTwitter,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "your degen pov",
+  description: "We're all degens here...",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <header className="flex justify-end pr-4 pb-0 mb-0 pt-2 bg-[#26437d] text-4xl">
+          <a
+            href="https://twitter.com/degenpovcto"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mr-2 text-[#ffff33] group"
+          >
+            <FontAwesomeIcon
+              icon={faXTwitter}
+              className="header-icon group-hover:hidden"
+            />
+            <FontAwesomeIcon
+              icon={faTwitter}
+              className="header-icon hidden group-hover:inline"
+            />
+          </a>
+          <a
+            href="https://t.me/degenpovcto"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[#ffff33]"
+          >
+            <FontAwesomeIcon icon={faTelegram} className="header-icon" />
+          </a>
+        </header>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
+
+export default function Home() {
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+
+  const handleCopy = (e: React.MouseEvent) => {
+    navigator.clipboard.writeText("0x4c96A67b0577358894407AF7Bc3158fC1DfFbeB5");
+    setTooltipPosition({ x: e.clientX - 75, y: e.clientY + 280 }); // Offset tooltip above the cursor
+    setTooltipVisible(true);
+    setTimeout(() => {
+      setTooltipVisible(false);
+    }, 2000); // Tooltip fades out after 2 seconds
+  };
+
+  return (
+    <main className="relative flex flex-col items-center justify-start min-h-screen bg-[#26437d] text-[#ffff33] p-4 pt-2 mt-0 pb-0 mb-0">
+      <div className="w-full pt-0 mt-0">
+        <Image
+          src="hero.png"
+          alt="Your Degen POV"
+          width={1920}
+          height={1080}
+          className="w-full h-auto"
+        />
+      </div>
+      <div className="mt-8 w-full max-w-2xl text-left pt-2 mt-0">
+        <p className="text-large font-bold">
+          {/* eslint-disable */}
+          “Oh, you can't help that,”
+          <br />
+          said the Cat
+          <br />
+          “we're all degens here. I'm a degen. You're a degen.”
+          <br />
+          “How do you know I'm a degen?”
+          <br />
+          said Alice.
+          <br />
+          “You must be,” said the “Cat, or you wouldn't have come here.”
+          {/* eslint-enable */}
+        </p>
+        <div
+          className="mt-4 font-bold flex items-center cursor-pointer relative"
+          onClick={handleCopy}
+        >
+          <span className="mr-2 text-xl">
+            CA 0x4c96A67b0577358894407AF7Bc3158fC1DfFbeB5
+          </span>
+          <FontAwesomeIcon icon={faCopy} />
+        </div>
+      </div>
+      {tooltipVisible && (
+        <div
+          className="absolute bg-gray-700 text-white p-1 rounded transition-opacity duration-500"
+          style={{
+            top: tooltipPosition.y, // Position above the mouse cursor
+            left: tooltipPosition.x,
+            whiteSpace: "nowrap", // Prevent new lines
+          }}
+        >
+          copied, you degen
+        </div>
+      )}
+    </main>
+  );
+}
