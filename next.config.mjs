2eb06b30d3ed1a46cf7e5fd6bbd3a71d40765268
FlyingMongoose <flyingmongoose@gmail.com>
2024-07-16 14:10:38 -0400

    fix: Fixed the website

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //reactStrictMode: true,
-  output: "export",
-  distDir: "deployment",
-  images: {
+    //reactStrictMode: true,
+    output: "export",
+    distDir: "deployment",
+    images: {
     unoptimized: true,
-  },
-};
-
-export default nextConfig;
+    },
+  };
+  
+  export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
 
   const handleCopy = (e: React.MouseEvent) => {
     navigator.clipboard.writeText("0x4c96A67b0577358894407AF7Bc3158fC1DfFbeB5");
-    setTooltipPosition({ x: e.clientX - 75, y: e.clientY + 280 }); // Offset tooltip above the cursor
+    setTooltipPosition({ x: e.clientX - 900, y: e.clientY - 850 }); // Increase the offset
     setTooltipVisible(true);
     setTimeout(() => {
       setTooltipVisible(false);
-    }, 2000); // Tooltip fades out after 2 seconds
+    }, 2000);
   };
 
   return (
@@ -26,7 +26,7 @@ export default function Home() {
     >
       <div className="w-full pt-0 mt-0">
         <Image
-          src="hero.png"
+          src="/hero.png"
           alt="Your Degen POV"
           width={1920}
           height={1080}
@@ -37,44 +37,56 @@ export default function Home() {
       <div className="mt-8 w-full max-w-2xl text-left pt-2 mt-0">
         <p className="text-l font-bold">
           {/* eslint-disable */}
-          “Oh, you can't help that,”
+          "Oh, you can't help that,"
           <br />
           said the Cat
           <br />
-          “we're all degens here. I'm a degen. You're a degen.”
+          "we're all degens here. I'm a degen. You're a degen."
           <br />
-          “How do you know I'm a degen?”
+          "How do you know I'm a degen?"
           <br />
           said Alice.
           <br />
-          “You must be,” said the Cat, “or you wouldn't have come here.”
+          "You must be," said the Cat, "or you wouldn't have come here."
           {/* eslint-enable */}
         </p>
-        <div
-          className="mt-4 pb-4 font-bold flex flex-col sm:flex-row items-center cursor-pointer relative justify-center"
-          onClick={handleCopy}
-        >
-          <span className="mr-2 text-2xl hover:underline ca-line text-center">
+        <div className="mt-4 pb-4 font-bold flex flex-col sm:flex-row items-center relative justify-center">
+          <a
+            href="https://basescan.org/token/0x4c96A67b0577358894407AF7Bc3158fC1DfFbeB5"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mr-2 text-2xl hover:underline ca-line text-center"
+          >
             CA 0x4c96A67b0577358894407AF7Bc3158fC1DfFbeB5
-          </span>
-          <FontAwesomeIcon
-            icon={faCopy}
-            className="mt-2 sm:mt-0 mb-2 sm:mb-0 pb-2 sm:pb-0"
-          />
+          </a>
+          <div className="relative">
+            <FontAwesomeIcon
+              icon={faCopy}
+              className="mt-2 sm:mt-0 mb-2 sm:mb-0 pb-2 sm:pb-0 cursor-pointer"
+              onClick={handleCopy}
+            />
+
+            {tooltipVisible && (
+  <div
+    className="absolute bg-gray-700 text-white p-2 rounded-lg transition-opacity duration-500"
+    style={{
+      top: tooltipPosition.y - 10,
+      left: tooltipPosition.x,
+      transform: 'translate(-50%, -100%)',
+      whiteSpace: "nowrap",
+    }}
+  >
+    <div className="relative">
+      copied, you degen
+      <div className="absolute w-3 h-3 bg-gray-700 transform rotate-45 left-1/2 -bottom-1.5 -ml-1.5"></div>
+    </div>
+  </div>
+)}
+
+
+          </div>
         </div>
       </div>
-      {tooltipVisible && (
-        <div
-          className="absolute bg-gray-700 text-white p-1 rounded transition-opacity duration-500"
-          style={{
-            top: tooltipPosition.y, // Position above the mouse cursor
-            left: tooltipPosition.x,
-            whiteSpace: "nowrap", // Prevent new lines
-          }}
-        >
-          copied, you degen
-        </div>
-      )}
     </main>
   );
 }
