e06609f7ed703d15a6f746dae2fbd79b4b103050
Lane Babuder <lane@tunedchaos.com>
2024-09-03 11:45:37 -0400

    chore: fixing footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <footer className="footer">
+          <p>Degen POV is a <a href="https://degenstudios.media/" target="_blank"><u>Degen Studios</u></a> project.</p>
+        </footer>
       </body>
     </html>
   );
